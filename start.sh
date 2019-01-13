48de30ffcfef7c2a3eb0845ce5e50a801bac2d71
subhaze <jmrussell22@gmail.com>
2019-01-13 23:15:33 +0000

    typo for bedrock server script

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-mv -v /tmp/resource_packs/* /bedrock-server/resource_packs/
-mv -v /tmp/behavoir_packs/* /bedrock-server/behavoir_packs/
+mv -v /tmp/resource_packs/* /bedrock-server/resource_packs/ 2>/dev/null
+mv -v /tmp/behavoir_packs/* /bedrock-server/behavoir_packs/ 2>/dev/null
 
-/bin/sh -c ./bedrock-server
+/bin/sh -c ./bedrock_server
