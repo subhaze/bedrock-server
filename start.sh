4b82882777f6622e499493eec24445d61953bc14
subhaze <jmrussell22@gmail.com>
2019-01-13 23:25:45 +0000

    typo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 mv -v /tmp/resource_packs/* /bedrock-server/resource_packs/ 2>/dev/null
-mv -v /tmp/behavoir_packs/* /bedrock-server/behavoir_packs/ 2>/dev/null
+mv -v /tmp/behavior_packs/* /bedrock-server/behavior_packs/ 2>/dev/null
 
 /bin/sh -c ./bedrock_server
