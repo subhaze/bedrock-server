657f751be3da1a077691621821a936ac7ae7ac04
subhaze <jmrussell22@gmail.com>
2019-01-14 00:02:14 +0000

    cp instead of mv

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-mv -v /tmp/resource_packs/* /bedrock-server/resource_packs/ 2>/dev/null
-mv -v /tmp/behavior_packs/* /bedrock-server/behavior_packs/ 2>/dev/null
+cp -a /tmp/resource_packs/. /bedrock-server/resource_packs/ 2>/dev/null
+cp -a /tmp/behavior_packs/. /bedrock-server/behavior_packs/ 2>/dev/null
 
 /bin/sh -c ./bedrock_server
